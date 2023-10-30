e509b45cf07995a0a0711ac049007806da36a6f2
Arin0508 <arinawasthi05@gmail.com>
2023-10-30 15:05:35 +0530

    Adding TestCases

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,33 +1,84 @@
-import java.beans.Transient;
-
 import org.example.Main;
-import org.junit.Before;
-import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 
 public class CalculatorTest {
-	private Main calculator;
-	@Before
-	public void setUp()
-	{
-		calculator = new Main();
-	}
+    private Main calculator;
 
-	@Test 
-	public void test_add_positive()
-	{
-		int a = 1;
-		int b = 2;
-		int expectedResult = 3;
-		Assert.assertEquals(expectedResult, calculator.Addition(a, b));	
-	}
-	@Test
-	public void test_add_negative()
-	{
-		int a = 1;
-		int b = 2;
-		int expectedResult = 0;
-		Assert.assertNotEquals(expectedResult, calculator.Subtraction(a, b));
-	}
+    @Before
+    public void setup(){ calculator = new Main(); }
+
+    @Test
+    public void test_add_positive()
+    {
+        double a = 1, b = 2;
+        double expec_res = 3;
+        double res = calculator.Addition(a, b);
+        Assert.assertEquals(expec_res, res, 0.000001);
+    }
+
+    @Test
+    public void test_add_negative()
+    {
+        double a = 1, b = 2;
+        double expec_res = 0;
+        double res = calculator.Addition(a, b);
+        Assert.assertNotEquals(expec_res, res);
+    }
+
+    @Test
+    public void test_sub_positive()
+    {
+        double a = 2, b = 2;
+        double expec_res = 0;
+        double res = calculator.Subtraction(a, b);
+        Assert.assertEquals(expec_res, res, 0.000001);
+    }
+
+    @Test
+    public void test_sub_negative()
+    {
+        double a = 2, b = 2;
+        double expec_res = 2;
+        double res = calculator.Subtraction(a, b);
+        Assert.assertNotEquals(expec_res, res);
+    }
+
+    @Test
+    public void test_mult_positive()
+    {
+        double a = 2, b = 3;
+        double expec_res = 6;
+        double res = calculator.Multiplication(a, b);
+        Assert.assertEquals(expec_res, res, 0.000001);
+    }
+
+    @Test
+    public void test_mult_negative() 
+    {
+        double a = 2, b = 3;
+        double expec_res = 1;
+        double res = calculator.Multiplication(a, b);
+        Assert.assertNotEquals(expec_res, res);
+    }
+
+    @Test
+    public void test_div_positive() 
+    {
+        double a = 2, b = 2;
+        double expec_res = 1;
+        double res = calculator.Division(a, b);
+        Assert.assertEquals(res, expec_res, 0.000001);
+    }
+
+    @Test
+    public void test_div_negative()
+    {
+        double a = 2, b = 2;
+        double expec_res = 0;
+        double res = calculator.Division(a, b);
+        Assert.assertNotEquals(res, expec_res);
+    }
 }
